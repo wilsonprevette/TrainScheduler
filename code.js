1fec4cb147bb451380546300a1ff3d72947b66da
Wilson P <wilson.prevette@gmail.com>
2018-06-16 00:07:43 -0500

    initial commit

---
+++ code.js
@@ -0,0 +1,15 @@
+<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBWmpN0ZkIQiXYVKFEA7e0ELlp9h1F92qo",
+    authDomain: "trainschedule-7a2e6.firebaseapp.com",
+    databaseURL: "https://trainschedule-7a2e6.firebaseio.com",
+    projectId: "trainschedule-7a2e6",
+    storageBucket: "",
+    messagingSenderId: "522211035942"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+</script>
