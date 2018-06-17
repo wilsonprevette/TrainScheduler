a7e48aadbba54363605fea949f24ed1d3f615195
Wilson P <wilson.prevette@gmail.com>
2018-06-17 17:57:30 -0500

    changes to js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,16 +1,39 @@
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBWmpN0ZkIQiXYVKFEA7e0ELlp9h1F92qo",
-    authDomain: "trainschedule-7a2e6.firebaseapp.com",
-    databaseURL: "https://trainschedule-7a2e6.firebaseio.com",
-    projectId: "trainschedule-7a2e6",
-    storageBucket: "",
-    messagingSenderId: "522211035942"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBWmpN0ZkIQiXYVKFEA7e0ELlp9h1F92qo",
+  authDomain: "trainschedule-7a2e6.firebaseapp.com",
+  databaseURL: "https://trainschedule-7a2e6.firebaseio.com",
+  projectId: "trainschedule-7a2e6",
+  storageBucket: "",
+  messagingSenderId: "522211035942"
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-</script>
+//set function to keep form from refreshing page
+$("submit").on("click", function (event) {
+  event.preventDefault();
+
+//set variables for form inputs
+  let train = $("inputTrain").val()
+  let desti = $("inputDest").val()
+  let time = $("inputTime").val()
+  let freq = $("inputFreq").val()
+
+//set firebase push function to append data
+  database.ref().push({
+    Name: train,
+    Destination: desti,
+    Time: time,
+    Frequency: freq
+  })
+
+  $("#inputTrain").val(" ");
+  $("#inputDest").val(" ");
+  $("#inputTime").val(" ");
+  $("#inputFreq").val(" ");
+}
+
+//set a function to have table capture values after submission
